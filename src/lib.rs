241b6235c62d3746f0c5c1399234440432394506
Guilherme Castelão <guilherme@castelao.net>
2024-01-27 08:33:24 -0800

    Intermediate layer get()
    Let's use get() when it gets the content in a buffer, and download when
    it actually saves the content somewhere.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,17 @@
 use reqwest::get;
 
+use reqwest;
+
 type Error = Box<dyn std::error::Error>;
 
-async fn download<W: std::io::Write>(url: &str, mut wtr: W) -> Result<usize, Error> {
-    //let url = "https://www.ngdc.noaa.gov/thredds/fileServer/global/ETOPO2022/60s/60s_bed_elev_netcdf/ETOPO_2022_v1_60s_N90W180_bed.nc";
-    //let hash = "e7e7efb75230280126bc96e910f71010";
-    // 491284376
-    //let url = "https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO2/ETOPO2v2-2006/ETOPO2v2g/netCDF/ETOPO2v2g_f4_netCDF.zip";
-    // https://raw.githubusercontent.com/castelao/asherpa/main/Cargo.toml
-
+// Should this have a size limit?
+async fn get<W: std::io::Write>(url: &str, wtr: &mut W) -> Result<usize, Error> {
     let mut res = reqwest::get(url).await?;
     let mut size = 0;
     while let Some(chunk) = res.chunk().await? {
         size += chunk.len();
         wtr.write_all(&chunk)?;
     }
-    /*
-        dbg!("filename", filename);
-        let mut fp = std::io::BufWriter::new(
-            std::fs::OpenOptions::new()
-                .create(true)
-                .write(true)
-                .open(filename)?,
-        );
-
-        let mut res = ureq::get(url).call()?.into_reader().take(500_000_000);
-        let mut buffer = vec![];
-        res.read_to_end(&mut buffer)?;
-        let s = fp.write(buffer.as_slice())?;
-        dbg!(s);
-    */
     Ok(size)
 }
 
