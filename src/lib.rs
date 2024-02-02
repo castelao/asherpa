12f63fc55a356436e3fc3fb2dbc57089d285d28c
Guilherme Castelão <guilherme@castelao.net>
2024-02-01 20:13:02 -0700

    Alpha (#1)
    * Adding dependencies
    
    * Working download()
    
    * Processing it in chunks
    
    * Adding tempfile to run tests
    
    * Intermediate layer get()
    
    Let's use get() when it gets the content in a buffer, and download when
    it actually saves the content somewhere.
    
    * Updating test for get() instead
    
    * Using futures
    
    * Minimum checks
    
    * Updating Cargo.lock
    
    * Missing metadata
    
    * Cleaning Cargo.lock
    
    * Using top level reqwest
    
    * fix: It was forcing test to fail for debuggingk
    
    * get() should be public
    
    * Ignoring target

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use reqwest;
+
+type Error = Box<dyn std::error::Error>;
+
+// Should this have a size limit?
+pub async fn get<W: std::io::Write>(url: &str, wtr: &mut W) -> Result<usize, Error> {
+    let mut res = reqwest::get(url).await?;
+    let mut size = 0;
+    while let Some(chunk) = res.chunk().await? {
+        size += chunk.len();
+        wtr.write_all(&chunk)?;
+    }
+    Ok(size)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn demo() {
+        let mut body = vec![];
+        {
+            let size = get(
+                "https://raw.githubusercontent.com/castelao/asherpa/main/Cargo.toml",
+                &mut body,
+            )
+            .await
+            .unwrap();
+            dbg!(size);
+        }
+    }
+}
