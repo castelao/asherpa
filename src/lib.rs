c1751c40bd1684f2a187485b535d13900c289069
Gui Castelao <guilherme@castelao.net>
2024-07-06 14:43:04 -0600

    refact: Renaming and re-organizing

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,13 @@ impl Artifact {
 }
 
 // Should this have a size limit?
-async fn get<W: std::io::Write>(url: &str, mut wtr: W) -> Result<usize, Error> {
+/// Read from a URL and write to a writer
+///
+/// # Parameters
+///
+/// - `url`: The URL to read from.
+/// - `wtr`: The writer to write to.
+async fn write<W: std::io::Write>(url: &str, mut wtr: W) -> Result<usize, Error> {
     tracing::debug!("Getting from: {}", url);
     let mut res = reqwest::get(url).await?;
     let mut size = 0;
@@ -26,20 +32,32 @@ async fn get<W: std::io::Write>(url: &str, mut wtr: W) -> Result<usize, Error> {
         tracing::trace!("Chunk size: {}", chunk.len());
         size += chunk.len();
         wtr.write_all(&chunk)?;
+        // wtr.write_all(&chunk).await?;
     }
+    tracing::debug!("Done writing {} bytes", &size);
     Ok(size)
 }
 
-async fn download<P: AsRef<Path>>(url: &str, filename: P) -> Result<usize, Error> {
-    tracing::debug!("Downloading from: {}", url);
+// Would a stream be more efficient here? As it is, it would first get
+// all the content before try to save it. Wouldn't be better to alternate
+// with chunks? Download a block and save it?
+/// Download one single artifact into a file
+async fn get<P: AsRef<Path>>(artifact: &Artifact, filename: P) -> Result<usize, Error> {
+    //let url = "https://www.ngdc.noaa.gov/thredds/fileServer/global/ETOPO2022/60s/60s_bed_elev_netcdf/ETOPO_2022_v1_60s_N90W180_bed.nc";
+    //let hash = "e7e7efb75230280126bc96e910f71010";
+    // 491284376
+    //let url = "https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO2/ETOPO2v2-2006/ETOPO2v2g/netCDF/ETOPO2v2g_f4_netCDF.zip";
+    tracing::debug!("Downloading: {:?}", artifact);
+    // let mut fp = tokio::fs::File::create(&filename).await?;
     let mut fp = std::io::BufWriter::new(
         std::fs::OpenOptions::new()
             .create_new(true)
             .write(true)
             .open(filename)?,
     );
+    //tracing::debug!("Will save artifact as: {:}", &filename);
 
-    let size = get(artifact.url(), &mut fp).await?;
+    let size = write(artifact.url(), &mut fp).await?;
     tracing::trace!("Downloaded {} bytes", size);
     fp.flush()?;
     tracing::trace!("Flushed");
@@ -48,12 +66,21 @@ async fn download<P: AsRef<Path>>(url: &str, filename: P) -> Result<usize, Error
 
 // Maybe we should target on having one single public function that
 // downloads a list of files asyncronously and return when all is done.
-fn demo(artifact: &Artifact, filename: &str) -> Result<String, Error> {
-    let mut path = dirs::cache_dir().unwrap();
-    path.push(filename);
+fn download(artifacts: Vec<Artifact>) -> Result<String, Error> {
+    let path = dirs::cache_dir().unwrap();
+    //path.push(filename);
     //dbg!(reqwest::Url::parse(url).unwrap().to_file_path());
     //use std::hash::{DefaultHasher, Hash, Hasher};
 
+    // Download all artifacts asyncronously
+    let tasks: Vec<_> = artifacts
+        .iter()
+        .map(|a| {
+            let mut p = path.clone();
+            p.push(a.filename.as_ref().unwrap());
+            get(a, p)
+        })
+        .collect();
     //let mut hasher = DefaultHasher::new();
     //url.hash(&mut hasher);
     //let filename = format!("{:x}", hasher.finish());
@@ -62,7 +89,13 @@ fn demo(artifact: &Artifact, filename: &str) -> Result<String, Error> {
         .enable_all()
         .build()
         .expect("Unable to create a runtime");
-    let _size = rt.block_on(download(artifact, &path))?;
+    let _size = rt.block_on(async {
+        // Use match and better error handling
+        futures::future::join_all(tasks)
+            .await
+            .into_iter()
+            .collect::<Vec<_>>()
+    });
     /*
     block_on(download(
         "https://raw.githubusercontent.com/castelao/asherpa/main/Cargo.toml",
@@ -101,7 +134,7 @@ mod tests {
     async fn demo_async() {
         let mut body = vec![];
         {
-            let size = get(
+            let size = write(
                 "https://raw.githubusercontent.com/castelao/asherpa/main/Cargo.toml",
                 &mut body,
             )
