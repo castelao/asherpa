5d037dec09b833ebfd88e9e474712054f94efe26
Gui Castelao <guilherme@castelao.net>
2024-07-05 18:45:17 -0600

    Tracing download

--- src/lib.rs
+++ src/lib.rs
@@ -23,17 +23,8 @@ async fn get<W: std::io::Write>(url: &str, mut wtr: W) -> Result<usize, Error> {
     Ok(size)
 }
 
-// Would a stream be more efficient here? As it is, it would first get
-// all the content before try to save it. Wouldn't be better to alternate
-// with chunks? Download a block and save it?
 async fn download<P: AsRef<Path>>(url: &str, filename: P) -> Result<usize, Error> {
-    //let url = "https://www.ngdc.noaa.gov/thredds/fileServer/global/ETOPO2022/60s/60s_bed_elev_netcdf/ETOPO_2022_v1_60s_N90W180_bed.nc";
-    //let hash = "e7e7efb75230280126bc96e910f71010";
-    // 491284376
-    //let url = "https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO2/ETOPO2v2-2006/ETOPO2v2g/netCDF/ETOPO2v2g_f4_netCDF.zip";
-    // https://raw.githubusercontent.com/castelao/asherpa/main/Cargo.toml
-
-    dbg!("Inside download");
+    tracing::debug!("Downloading from: {}", url);
     let mut fp = std::io::BufWriter::new(
         std::fs::OpenOptions::new()
             .create_new(true)
